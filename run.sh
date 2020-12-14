02bda4a3f07c92c180fe420423356d2e390fc2b2
Jay <44664519+Eat-Sleep-Code-fun@users.noreply.github.com>
2020-12-15 00:25:48 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl -H Metadata:true http://169.254.169.254/metadata/instance?api-version=2020-09-01 | ConvertTo-Json
+curl -H Metadata:true http://169.254.169.254/metadata/instance?api-version=2020-09-01
